65c7ad0d763e6d887bfb845b10ec1486932e6136
amoghrathod <amoghrrathod@gmail.com>
2024-10-01 01:52:44 +0530

    Update brew

--- Formula/alertify.rb
+++ Formula/alertify.rb
@@ -4,11 +4,14 @@ class Alertify < Formula
   url "https://github.com/amoghrrathod/alertify/releases/download/v1.0.1/alertify-1.0.1.tar.gz"
   sha256 "e00f1ec48ef7cc3f5237f3669ec5c505d8814d529e873e7038bad05fcbce2812"
 
+  depends_on "jansson" # Dependency for JSON parsing
+
   def install
-    system "make", "PREFIX=#{prefix}"
+    system "make" # Compile the binary
+    bin.install "alertify" # Install the compiled binary into Homebrew's bin directory
   end
 
   test do
-    system "#{bin}/alertify", "--version"
+    system "#{bin}/alertify", "--version" # Test to check if it installed correctly
   end
 end
